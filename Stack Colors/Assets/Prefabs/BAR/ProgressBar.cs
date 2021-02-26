afbc3148e4bc54618bad80649db3c1d6c9d3d478
TickLuck <60887607+LuckyDSh@users.noreply.github.com>
2021-02-26 05:03:26 +0500

    Initial Commit

---
+++ Stack Colors/Assets/Prefabs/BAR/ProgressBar.cs
@@ -0,0 +1,56 @@
+﻿/*
+* TickLuck Team
+* All rights reserved
+*/
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour
+{
+    #region Variables 
+    [HideInInspector] public static Slider s_meltBarSlider;
+    public static float maxTime = 100f; 
+    public Gradient gradient;
+    public Image fill;
+
+    [HideInInspector]
+    public static bool isOn;
+    #endregion
+
+    #region UnityMethods
+    private void Start()
+    {
+        maxTime = 0;
+        
+        s_meltBarSlider = gameObject.GetComponent<Slider>();
+        s_meltBarSlider.value = maxTime;
+        isOn = true;
+       
+    }
+
+    private void Update()
+    {
+        if (isOn)
+        {
+            maxTime = PlayerStackColorController.forwardForce;
+            SetProgress(maxTime);
+        }
+    }
+    #endregion
+
+    public void SetMaxValue(int maxvalue)
+    {
+        s_meltBarSlider.maxValue = maxvalue;
+        s_meltBarSlider.value = maxvalue;
+
+        fill.color = gradient.Evaluate(1f);
+    }
+
+    public void SetProgress(float value)
+    {
+        s_meltBarSlider.value = value;
+
+        fill.color = gradient.Evaluate(s_meltBarSlider.normalizedValue);//
+    }
+}
---
+++ Stack Colors/Assets/Scripts/PlayerStackColorController.cs
@@ -0,0 +1,218 @@
+﻿/*
+*	TickLuck
+*	All rights reserved
+*/
+using System;
+using UnityEngine;
+
+public class PlayerStackColorController : MonoBehaviour
+{
+    #region Variables
+    [SerializeField] private Color _color;
+    [SerializeField] private Renderer[] _renderers;
+
+    [SerializeField] public static bool is_Playing;
+    [SerializeField] private float forwardSpeed;
+    [SerializeField] private float moveSideWaysLerp;
+
+    private Rigidbody rb;
+
+    private Transform parentPickUp;
+    public static Transform ParentPickUp_buffer;
+    [SerializeField] private Transform stackPosition;
+
+    public static bool atEnd;
+    [SerializeField] private GameObject forceAmountBar;
+    [SerializeField] public static float forwardForce; // static
+    [SerializeField] private float forceAdder;
+    [SerializeField] private float forceReducer;
+
+    public static Color PlayerColor_buffer;
+    public static Action<float> Kick;
+    #endregion
+
+    #region UnityMethods
+    void Start()
+    {
+        atEnd = false;
+        forceAmountBar.SetActive(false);
+        int rand = UnityEngine.Random.Range(0, ColorController.s_pickUpColors_Buffer.Count);
+        rb = GetComponent<Rigidbody>();
+        SetColor(ColorController.s_pickUpColors_Buffer[rand].Color);
+    }
+
+    void Update()
+    {
+        PlayerColor_buffer = _color;
+        ParentPickUp_buffer = parentPickUp;
+
+        if (is_Playing)
+            MoveForward();
+
+        if (atEnd)
+        {
+            forwardForce -= forceReducer * Time.deltaTime;
+            if (forwardForce < 0)
+                forwardForce = 0;
+        }
+
+        #region PC Control
+        if (Input.GetMouseButtonDown(0))
+            if (atEnd)
+                forwardForce += forceAdder;
+
+        if (Input.GetMouseButton(0)) 
+        {
+            if (atEnd)
+                return;
+
+            if (is_Playing == false)
+                is_Playing = true;
+            MoveSideWays_usingMouse();
+        }
+        #endregion
+
+        #region Phone Control
+        if (Input.touchCount > 0)
+            if (atEnd)
+                forwardForce += forceAdder;
+
+        if (Input.touchCount > 0)
+        {
+            if (atEnd)
+                return;
+
+            if (is_Playing == false)
+                is_Playing = true;
+            MoveSideWays_usingTouch();
+        }
+        #endregion
+    }
+    #endregion
+
+    private void MoveSideWays_usingTouch()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+        RaycastHit hitInfo;
+        if (Physics.Raycast(ray, out hitInfo, 100))
+        {
+            transform.position = Vector3.Lerp(transform.position,
+                new Vector3(hitInfo.point.x, transform.position.y, transform.position.z), moveSideWaysLerp * Time.deltaTime);
+        }
+    }
+
+    private void MoveSideWays_usingMouse()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+        if (Physics.Raycast(ray, out hitInfo, 100))
+        {
+            transform.position = Vector3.Lerp(transform.position,
+                new Vector3(hitInfo.point.x, transform.position.y, transform.position.z), moveSideWaysLerp * Time.deltaTime);
+        }
+    }
+
+    private void MoveForward()
+    {
+        rb.velocity = Vector3.forward * forwardSpeed;
+    }
+
+    private void SetColor(Color color)
+    {
+        _color = color;
+        for (int i = 0; i < _renderers.Length; i++)
+        {
+            _renderers[i].material.SetColor("_Color", _color);
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        #region FINISH ENTERED
+        if (other.tag == "FinishLineStart")
+        {
+            forceAmountBar.SetActive(true);
+            atEnd = true;
+        }
+        if (other.tag == "FinishLineEnd")
+        {
+            rb.velocity = Vector3.zero;
+            is_Playing = false;
+            forceAmountBar.SetActive(false);
+            LaunchStack();
+        }
+        #endregion
+
+        if (atEnd)
+            return;
+
+        if (other.tag == "Pickup")
+        {
+            Transform otherTransform = other.transform;
+
+            if (other.transform.parent != null)
+                otherTransform = other.transform.parent;
+
+            if (_color == other.GetComponent<Pickup_Controller>().PickUp_color)
+                GAME_CONTROLLER.instance.UpdateScore(otherTransform.GetComponent<Pickup_Controller>().value_buffer);
+            else
+            {
+                #region WRONG PICKUP CHOSEN
+                GAME_CONTROLLER.instance.UpdateScore(otherTransform.GetComponent<Pickup_Controller>().value_buffer * -1);
+                Destroy(other.gameObject);
+                if (parentPickUp != null)
+                {
+                    if (parentPickUp.childCount > 1)
+                    {
+                        parentPickUp.position -= Vector3.up * parentPickUp.GetChild(parentPickUp.childCount - 1).localScale.y;
+                        Destroy(parentPickUp.GetChild(parentPickUp.childCount - 1).gameObject);
+                    }
+                    else
+                        Destroy(parentPickUp.gameObject);
+                }
+
+                return;
+                #endregion
+            }
+
+            #region SETTINGS
+            Rigidbody otherRB = otherTransform.GetComponent<Rigidbody>();
+            otherRB.isKinematic = true;
+            other.enabled = false;
+            other.gameObject.GetComponent<Pickup_Controller>().is_Picked = true;
+            #endregion
+
+            if (parentPickUp == null)
+            {
+                #region SET NEW PICKUP PARENT
+                parentPickUp = otherTransform;
+                parentPickUp.position = stackPosition.position;
+                parentPickUp.parent = stackPosition;
+                #endregion
+            }
+            else
+            {
+                #region PLACE PICKUP ON TOP OF PARENT
+                parentPickUp.position += Vector3.up * (otherTransform.localScale.y);
+                otherTransform.position = stackPosition.position;
+                otherTransform.parent = parentPickUp;
+                #endregion
+            }
+
+        }
+    }
+
+    private void LaunchStack()
+    {
+        Camera.main.GetComponent<CameraMovementController>().Target = parentPickUp;
+        Kick(forwardForce);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "ColorWall")
+        {
+            SetColor(other.GetComponent<ColorWallController>().NewColor);
+        }
+    }
+}
